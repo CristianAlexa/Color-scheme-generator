c2452c7f8c775f65258f0b9e8bba68de0f3478af
CristianAlexa <94307268+CristianAlexa@users.noreply.github.com>
2023-07-31 20:33:50 +0100

    mobile version fixed, error fixed and loged
    mobile responsive - fixed
    1 api bug - fixed
    1 api bug kept to be logged in the DOM

--- app.js
+++ app.js
@@ -12,9 +12,9 @@ class App {
   addEventListeners() {
     this.getColorsBtn.addEventListener("click", () => {
       const color = this.colorInputEl.value.slice(1);
-      const scheme = this.colorModeEl.value.toLowerCase();
+      const scheme = this.colorModeEl.value;
       const APIUrl =
-        this.API + "/scheme?hex=" + color + "&mode=" + scheme + "&count=5";
+        this.API + "/scheme?hex=" + color + "&mode=" + scheme + "&count=6";
       this.getColorsData(APIUrl);
     });
   }
@@ -22,7 +22,8 @@ class App {
   getColorsData(url) {
     fetch(url)
       .then((res) => res.json())
-      .then((data) => this.getColorDataHtml(data));
+      .then((data) => this.getColorDataHtml(data))
+      .catch((err) => this.handleError(err));
   }
 
   getColorDataHtml(data) {
@@ -37,6 +38,15 @@ class App {
             </li>
       `;
     });
+    this.colorsResultEl.innerHTML = colorsResultHtml;
+  }
+
+  handleError() {
+    let colorsResultHtml = `
+            <li class="colors" id="error">
+            <h2 class="color-code">Error: Cannot read property of 'scheme type'.</h2>
+            </li>
+    `;
     this.colorsResultEl.innerHTML = colorsResultHtml;
   }
 }
